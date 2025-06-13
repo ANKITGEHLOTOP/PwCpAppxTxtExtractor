ef4f81913afe8feffbab0cd25791e5f4cb9c9468
ANKITGEHLOTOP <technicalankit123@gmail.com>
2025-06-13 12:00:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "29905645"
+api_hash = "e5a701f6e0b5fb659cb57a230b9a3feb"
+bot_token = "7577699921:AAFOL8CUzT2JzbUth4Oed6A88680WKadXUI"
+auth_users = [6743675415]
